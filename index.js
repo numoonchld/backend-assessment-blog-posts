09a509f26ebe16bccfef12515485be3181aa7a3b
numoonchld <raghavendra.saralaya@gmail.com>
2021-02-24 05:06:42 +0530

    unique filtering logic progress save

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // import dependencies ------------------------------------------------------------
 const express = require('express')
 const cors = require('cors')
+const fetch = require('node-fetch')
 
 // initialization ------------------------------------------------------------
 const app = express()
@@ -10,10 +11,47 @@ const app = express()
 app.use(cors()) // use cors in app 
 app.use(express.json()) // get only body of request from client side
 
+// process data ---------------------------------------------------------------
+function combineAndUnique(cache, queryTagsArray) {
+
+    const fullCacheDeepClone = JSON.parse(JSON.stringify(cache))
+
+    if (queryTagsArray.length === 1) {
+
+        return cache[queryTagsArray[0]]
+
+    } else {
+
+        const postTagComboSet = new Set()
+        let mergedPostArrays = []
+        let finalPostsArray = []
+
+        for (const queryTag of queryTagsArray) {
+            console.log('LOG 0: ', queryTag, fullCacheDeepClone[queryTag].length)
+
+            mergedPostArrays = mergedPostArrays.concat(fullCacheDeepClone[queryTag])
 
 
+            for (const post of fullCacheDeepClone[queryTag]) {
+                // console.log(post.id, post)
+                postTagComboSet.add(post.id)
 
-// load data ------------------------------------------------------------
+            }
+        }
+
+        console.log('LOG 0: ', postTagComboSet.size, mergedPostArrays.length)
+
+        for (const postID of postTagComboSet) {
+            
+            uniquePost = mergedPostArrays.filter(post => post.id === postID)[0]
+            finalPostsArray.push(uniquePost)
+
+        }
+        console.log(finalPostsArray.length)
+        return finalPostsArray
+    }
+
+}
 
 //// api end points ------------------------------------------------------------
 // route 1: /api/ping, GET
@@ -36,6 +74,80 @@ app.get('/api/ping', async (req, res) => {
         res.json(responsePayload)
     }
 })
+
+// route 2: /api/posts
+let cache = {}
+app.get('/api/posts', async (req, res) => {
+    try {
+
+        // tags query check
+        if (!req.query.tags) {
+            res.status(500)
+            res.send({ error: 'Tags parameter is required' })
+        } else {
+            queryItems = req.query
+            console.log('LOG 1: ', queryItems)
+
+
+            // cache tag API data
+            const queryTagsArray = req.query.tags.split(',')
+
+            for (const queryTag of queryTagsArray) {
+
+                if (!cache[queryTag]) {
+
+                    console.log('No Cache found! - Creating cache entry for given tag query.')
+
+                    await fetch(`https://api.hatchways.io/assessment/blog/posts?tag=${queryTag}`)
+                        .then(response => response.json())
+                        .then(payload => {
+                            cache[queryTag] = payload.posts
+                        })
+                } else {
+
+                    console.log('Query found in cache! - Using that for further processing.')
+
+                }
+            }
+
+            // console.log(cache)
+
+            // sort by phase 
+
+            const sortByParam = req.query.sortBy
+            console.log(sortByParam)
+
+            const direction = req.query.direction
+            console.log(direction)
+
+            if (!sortByParam && !direction) {
+
+                const finalPostsArray = combineAndUnique(cache, queryTagsArray)
+
+                res.send({ posts: finalPostsArray })
+
+            } else if (!sortByParam && direction) {
+
+            } else if (sortByParam && !direction) {
+
+            } else if (sortByParam && direction) {
+
+            } else {
+                res.send({ info: 'End-point under construction!' })
+            }
+
+
+
+
+        }
+
+
+
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+
 
 //  server listen 
 app.listen(3000, '0.0.0.0', () => {
--- test.js
+++ test.js
@@ -1,21 +1,54 @@
+'use-strict'
 // test suite hook-up -------------------------------------------------------
 const chai = require('chai')
 const chaiHttp = require('chai-http')
-const expect = require('chai').expect
+const assert = require('chai').assert
 
 const server = require('./index')
 
 chai.use(chaiHttp)
 
-describe('Blobs', function () {
-    it('should get success true on ping')
-})
-
-it('should get success true on ping', function (done) {
+it('success true on ping /GET', function (done) {
     chai.request(server)
         .get('/api/ping')
         .end(function (err, res) {
-            expect(res).to.have.status(200)
-            done()
+            if (!err) {
+                assert.equal(res.statusCode, 200)
+                assert.equal(res.body.success, true)
+                done()
+            } else {
+                console.log(err)
+            }
+
+        })
+})
+
+it('throws error when no query params are sent to /api/posts /GET', function (done) {
+    chai.request(server)
+        .get('/api/posts')
+        .end(function (err, res) {
+            if (!err) {
+                assert.equal(res.statusCode, 500)
+                assert.equal(res.body.error, 'Tags parameter is required')
+                done()
+            } else {
+                console.log(err)
+            }
+
+        })
+})
+
+
+it('tags - 1 /api/posts?tags=tech /GET', function (done) {
+    chai.request(server)
+        .get('/api/posts?tags=tech')
+        .end(function (err, res) {
+            if (!err) {
+                assert.equal(res.statusCode, 200)
+                done()
+            } else {
+                console.log(err)
+            }
+
         })
 })
